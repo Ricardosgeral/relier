cdc61a56f7d3493a0cda2dfe34bd2ab86e91c5b0
Ricardosgeral <ricardos.geral@gmail.com>
2018-05-10 10:21:16 +0100

    update name

--- bash/launcher.sh
+++ bash/launcher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # launcher.sh
-# navigate to home directory, then to this directory, then execute python3scrip$
-Sleep 13            # required to allow complete boot of raspbian
-cd /home/pi/LerAS
+# navigate to home directory, then to this directory, then execute python3 scrip$
+sleep 8            # required to allow complete boot of Raspbian
+cd /home/pi/relier
 python3 main.py
