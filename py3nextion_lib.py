2f221ecfd1448599fb971195b5d14eb010f9aae0
Ricardosgeral <ricardos.geral@gmail.com>
2018-05-11 17:24:24 +0100

    readme update

--- py3nextion_lib.py
+++ py3nextion_lib.py
@@ -3,7 +3,7 @@
 # import serial
 # ser = serial.Serial(
 #   port='/dev/ttyAMA0',
-#   baudrate = 38400,
+#   baudrate = 9600,
 #   parity=serial.PARITY_NONE,
 #   stopbits=serial.STOPBITS_ONE,
 #   bytesize=serial.EIGHTBITS,
