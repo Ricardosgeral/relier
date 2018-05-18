4e8125367fe2f40b701d23ac6033dbe467165064
Ricardosgeral <ricardos.geral@gmail.com>
2018-05-18 15:46:06 +0100

    readme update

--- bash/raspbian-post-install.sh
+++ bash/raspbian-post-install.sh
@@ -14,12 +14,12 @@ if [ "$(id -u)" != 0 ]; then
   exit 1
 fi
 clear
-
+#
 echo '------------------------------------------------------------------------'
 echo '=> Raspbian post-install script for relier... pick a very hot cup of tea'
 echo '------------------------------------------------------------------------'
 echo '------------------------------------------------------------------------'
-
+#
 echo '---------------------------'
 echo '=> System update/upgrade   '
 echo '---------------------------'
@@ -28,32 +28,32 @@ sudo apt-get update -qq
 echo '=> Performe system upgrade'
 sudo apt-get dist-upgrade -y
 echo 'Done.'
-
+#
 echo  '---------------------------------------------------------------------------'
 echo  '=> Folder to save the data values from sensors csv (comma separated values)'
 echo  '---------------------------------------------------------------------------'
-
+#
 if [ -d "/srv/EROSTESTS/" ]; then
     echo 'Directory /srv/EROSTESTS/ already exists'
 else
     sudo mkdir /srv/EROSTESTS
 fi
-
+#
 if [ -d "/media/pi/" ]; then
     echo 'Directory /media/pi/ already exists'
 else
     sudo mkdir /media/pi/
 fi
-
+#
 # permissions
 sudo chmod -R 777 /srv/EROSTESTS
 sudo chown pi:pi /media/pi
 sudo chmod -R 777 /media/pi
-
+#
 echo '------------------------------'
 echo ' => Get repository from Gibhub'
 echo ' -----------------------------'
-
+#
 if [ -d "/home/pi/relier" ]; then
     sudo rm -R /home/pi/relier
 fi
@@ -61,14 +61,14 @@ git clone https://github.com/Ricardosgeral/relier.git /home/pi/relier
 #these following two lines may be unnecessary
 sudo chown -R pi: /home/pi/relier
 sudo chmod 777 -R relier/
-
+#
 echo  '---------------------------'
 echo  '=> Install system utilities'
 echo  '---------------------------'
 echo '=> Install system utilities'
 #sudo apt-get install -y .....whatever you want e.g. latex
 echo 'Done.'
-
+#
 echo  '------------------------------------------'
 echo  '=> Install developer packages (python 3.5)'
 echo  '------------------------------------------'
@@ -83,19 +83,19 @@ sudo pip3 install RPi.bme280
 sudo pip3 install w1thermsensor
 #sudo pip3 install --upgrade pyasn1-modules # required for pandas but don't know why!!!
 echo 'Done.'
-
+#
 echo '---------------------'
 echo '=> Enable SSH and VNC'
 echo '---------------------'
-
+#
 echo '>>> Enable SSH and VNC'
 sudo raspi-config nonint do_ssh 0
 sudo raspi-config nonint do_vnc 0
-
+#
 echo  '--------------------------------------------------'
 echo  '=> Configurations for serial, 1-Wire, I2C and UART'
 echo  '--------------------------------------------------'
-
+#
 # enable 1-Wire GPIO on Raspberry Pi
 echo '>>> Enable 1-Wire GPIO (w1)'
 if grep -q 'dtoverlay=w1-gpio' /boot/config.txt; then
@@ -103,7 +103,7 @@ if grep -q 'dtoverlay=w1-gpio' /boot/config.txt; then
 else
   echo dtoverlay=w1-gpio >> /boot/config.txt
 fi
-
+#
 # enable I2C on Raspberry Pi
 echo '>>> Enable I2C'
 sudo raspi-config nonint do_i2c 0
@@ -118,7 +118,7 @@ else
   echo 'dtparam=i2c1=on' >> /boot/config.txt
 fi
 echo 'dtparam=i2c_baudrate=400000' >> /boot/config.txt
-
+#
 # configure UART (Enable pins 14 and 15 to ttyAMA0 and Bluetooth to miniuart (which has limitations))
 # start by disabling the serial console
 echo '>>> Disable serial console'
@@ -135,43 +135,43 @@ if grep -q 'dtoverlay=pi3-disable-bt' /boot/config.txt; then
 else
   echo dtoverlay=pi3-disable-bt >> /boot/config.txt
 fi
-
+#
 #Stop Bluetooth modem to use UART
 sudo systemctl disable hciuart
-
+#
 echo  '----------------------------------------------------------------'
 echo  '=> Create systemd unit file for hotplugging (mount/unmount) usb'
 echo  '----------------------------------------------------------------'
-
+#
 #Create a script to run at power up
 sudo cp /home/pi/relier/bash/usb-mount.sh /usr/local/bin/usb-mount.sh
 sudo sed -i 's/\r//' /usr/local/bin/usb-mount.sh
-
+#
 #Call the script by a system unit file
 sudo cp /home/pi/relier/services/usb-mount@.service /etc/systemd/system/usb-mount@.service
-
+#
 #Add the two rules (file 99-local.rules) to start and stop system unit service on hotplug/unplug
 sudo echo 'KERNEL== "sd[a-z][0-9]", SUBSYSTEMS=="usb", ACTION=="add", RUN+="/bin/systemctl start usb-mount@%k.service"' >> /etc/udev/rules.d/99-local.rules
 sudo echo 'KERNEL=="sd[a-z][0-9]", SUBSYSTEMS=="usb", ACTION=="remove", RUN+="/bin/systemctl stop usb-mount@%k.service"'>> /etc/udev/rules.d/99-local.rules
-
+#
 sudo udevadm control -l debug           #allows debugging (tail –f /var/log/syslog, and plug/unplug a usb stick)
 sudo udevadm control --reload-rules     #reloads rules
 sudo systemctl daemon-reload            #reloads systemd
-
+#
 echo '--------------------------------------------------------'
 echo ' => Create systemd unit file to run at shutdown/reboot'
 echo ' -------------------------------------------------------'
-
+#
 #Create a script to run at shutdown/reboot
 sudo cp /home/pi/relier/services/rcshut.service /etc/systemd/system/rcshut.service
 sudo systemctl enable rcshut --now
 sudo systemctl start rcshut
 sudo chmod +w /home/pi/relier/shutdown.py
-
+#
 echo  '----------------------------------------------------------------'
 echo  '=> Create systemd unit file to control shutdown/restart button'
 echo  '----------------------------------------------------------------'
-
+#
 #Create a script to run at shutdown/reboot
 #sudo apt install python3-gpiozero this should be not needed because is already in raspbian now
 sudo cp /home/pi/relier/services/shutdown_button.service /etc/systemd/system/shutdown_button.service
@@ -179,14 +179,14 @@ sudo systemctl daemon-reload
 sudo systemctl enable shutdown_button --now
 sudo systemctl start shutdown_button
 sudo chmod +w /home/pi/relier/shutdown_button.py
-
+#
 echo  '---------------------------------------------------------'
 echo  '=> Configurations of cron tab (files to run at startup)'
 echo  '---------------------------------------------------------'
-
+#
 # First remove all cron's
 crontab -r
-
+#
 CMD="/usr/bin/pigpiod"
 JOB="@reboot $CMD"
 TMPC="mycron1"
@@ -200,19 +200,19 @@ sudo chmod 755 /home/pi/relier/bash/launcher.sh
 echo 'Create a logs directory'
 sudo mkdir /home/pi/relier/logs
 sudo chmod 777 -R /home/pi/relier/logs
-
+#
 CMD="sh /home/pi/relier/bash/launcher.sh >/home/pi/relier/logs/cronlog 2>&1"
 JOB="@reboot $CMD"
 TMPC="mycron2"
 sudo grep "$CMD" -q <(crontab -l) || (crontab -l>"$TMPC"; echo "$JOB">>"$TMPC"; crontab "$TMPC")
-
+#
 echo  '--------------------'
 echo  '=> Final reboot'
 echo  '--------------------'
-
+#
 if [ -f "/tmp/raspbian-post-install.sh" ]; then
 sudo rm /tmp/raspbian-post-install.sh
 fi
 echo '>> Rebooting'
-
+#
 sudo reboot
