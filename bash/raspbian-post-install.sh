11ffbe9761122d50d705f6fd1753cd01a386a43a
Ricardos <ricardos.geral@gmail.com>
2018-06-11 17:38:24 +0100

    setup include python3.6 and dash

--- bash/raspbian-post-install.sh
+++ bash/raspbian-post-install.sh
@@ -73,20 +73,11 @@ echo  '------------------------------------------'
 echo  '=> Install developer packages (python 3.5)'
 echo  '------------------------------------------'
 echo '=> Install developer packages'
-sudo apt-get update
-sudo apt-get install -y python3-pandas
-# install python3.6
-#If one of the packages cannot be found, try a newer version number (e.g. libdb5.4-dev instead of libdb5.3-dev).
-sudo apt-get -y install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
-sudo wget https://www.python.org/ftp/python/3.6.5/Python-3.6.5.tar.xz
-sudo tar xf Python-3.6.5.tar.xz
-sudo cd Python-3.6.5
-sudo ./configure
-sudo make
-sudo make altinstall
+#
+sudo pip3 install --upgrade pip
 sudo pip3 install pygsheets
+sudo pip3 install --upgrade oauth2client
 sudo pip3 install --upgrade pyasn1-modules # required for pysheets !!!
-sudo pip3 install bokeh
 sudo pip3 install configparser
 sudo pip3 install https://github.com/nithinmurali/pygsheets/archive/master.zip
 sudo pip3 install adafruit-ads1x15
@@ -225,26 +216,34 @@ sudo grep "$CMD" -q <(crontab -l) || (crontab -l>"$TMPC"; echo "$JOB">>"$TMPC"; 
 echo 'Done.'
 #
 echo  '----------------------------------------------------'
-echo  '=> Install python3.6 and Dash (works with python3.6)'
+echo  '=> Install python3.6  (for Dash and pandas with python3.6)'
 echo  '----------------------------------------------------'
 #
 #If one of the packages cannot be found, try a newer version number (e.g. libdb5.4-dev instead of libdb5.3-dev).
-sudo apt-get install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
-sudo wget https://www.python.org/ftp/python/3.6.5/Python-3.6.5.tar.xz
-sudo tar xf Python-3.6.5.tar.xz
-sudo cd Python-3.6.5
-sudo ./configure
-sudo make
+sudo apt-get -y install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
+wget https://www.python.org/ftp/python/3.6.5/Python-3.6.5.tar.xz
+tar xf Python-3.6.5.tar.xz
+./Python-3.6.5/configure
+make
 sudo make altinstall
 sudo rm -r Python-3.6.5
 sudo rm Python-3.6.5.tar.xz
-sudo apt-get -y --purge remove build-essential tk-dev
-sudo apt-get -y --purge remove libncurses5-dev libncursesw5-dev libreadline6-dev
-# Adjust version numbers if necessary
-sudo apt-get -y --purge remove libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev
-sudo apt-get -y --purge remove libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
-sudo apt-get -y autoremove
-sudo apt-get -y clean
+sudo pip3.6 install --upgrade pip
+sudo pip3.6 install numpy
+sudo pip3.6 install pandas
+sudo pip3.6 install dash==0.21.1  # The core dash backend
+sudo pip3.6 install dash-renderer==0.13.0  # The dash front-end
+sudo pip3.6 install dash-html-components==0.11.0  # HTML components
+sudo pip3.6 install dash-core-components==0.23.0  # Supercharged components
+sudo pip3.6 install plotly --upgrade  # Plotly graphing library used in examples
+## some cleaning
+#sudo apt-get -y --purge remove build-essential tk-dev
+#sudo apt-get -y --purge remove libncurses5-dev libncursesw5-dev libreadline6-dev
+## Adjust version numbers if necessary
+#sudo apt-get -y --purge remove libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev
+#sudo apt-get -y --purge remove libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
+#sudo apt-get -y autoremove
+#sudo apt-get -y clean
 #
 echo  '--------------------'
 echo  '=> Final reboot'
