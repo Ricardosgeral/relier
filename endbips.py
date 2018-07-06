1bc4ef89d3286948ee865443fc7752e8e8884436
Ricardosgeral <ricardos.geral@gmail.com>
2018-07-06 11:46:05 +0100

    correct buzer pin

--- endbips.py
+++ endbips.py
@@ -4,7 +4,7 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-buzzpin=12 # (GPIO18)
+buzzpin=18 # (GPIO18)
 GPIO.setup(buzzpin,GPIO.OUT)
 loop = 2 # number of morse code loops
 
@@ -55,6 +55,7 @@ def morsecode_bips ():
 def test_end():
     for i in range(0,loop):
         morsecode_bips ()
+
 
 #os.system('clear')
 #test_end()
