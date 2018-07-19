577bafbd93f74272e001fbade75f904aff192455
Ricardosgeral <ricardos.geral@gmail.com>
2018-07-19 16:24:52 +0100

    turbidity bug in google docs corrected

--- sensor_server.py
+++ sensor_server.py
@@ -219,7 +219,7 @@ def get_data(interval, mu, mi, md, bu, bi, bd, mturb, bturb, zerou, zeroi, zerod
         'mmH2O_int':    round(mmH2O[PRESSINT_ch]),
         'mmH2O_down':   round(mmH2O[PRESSDW_ch]),
         'ana_turb':     round(analog[TURB_ch]), #analog number
-        'turb':     round(turb,2),
+        'turb':         round(turb,2),
         'flow':         round(flowrate,2),
         'liters':       round(total_liters),
         'water_temp':   round(water_temp,1),
