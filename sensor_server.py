65053f4cd8ba402ca6d9fa2629432c20b42a8960
Ricardosgeral <ricardos.geral@gmail.com>
2018-11-14 16:50:00 +0000

    make sure DATARATE in ADS1115 is the same in both files "sensor_server.py" and "analogsensor_thread.py"

--- sensor_server.py
+++ sensor_server.py
@@ -26,7 +26,7 @@ It also records the time and date of the measure.
 
 # Libraries required
 from time import sleep
-from analogsensor_thread import AnalogSensor, GAIN
+from analogsensor_thread import AnalogSensor, GAIN, DATA_RATE
 from digitalSen_thread import D_Temp
 from datetime import datetime
 from math import log as log
@@ -110,7 +110,7 @@ def zero_press(mu, mi, md, bu, bi, bd, testtype):
     zerou  = zeroi = zerod = 0 # values to zero pressure sensors - datum
     analog = [0]*3
     for ch in range(3):
-        analog[ch] = adc.read_adc(ch, gain=GAIN, data_rate=860)  # check if it's equal to Analogsensor.py
+        analog[ch] = adc.read_adc(ch, gain=GAIN, data_rate=DATA_RATE)
         # transformar o valor lido pelo adc em volts
         # Ratio of 15 bit value to max volts determines volts
         volts[ch] = analog[ch] / 32767.0 * max_VOLT
