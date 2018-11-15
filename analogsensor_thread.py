3fe7ae64f84fd90bc6850acda73e288e986e02a2
Ricardosgeral <ricardos.geral@gmail.com>
2018-11-15 18:46:28 +0000

    minor

--- analogsensor_thread.py
+++ analogsensor_thread.py
@@ -17,7 +17,6 @@ adc = Adafruit_ADS1x15.ADS1115(address=0x48, busnum=1) #address of ADC See in --
 #  -   8 = +/-0.512V
 #  -  16 = +/-0.256V
 
-# CHECK IS IT'S THE SAME AS IN SENSOR_SERVER.PY
 # See table 3 in the ADS1015/ADS1115 datasheet for more info on gain.
 GAIN = 1  #2/3, 1, 2, 4 , 6 , 8, 16
 DATA_RATE = 250  # 8, 16, 32, 64, 128, 250, 475, 860 samples/second   max =860 the higher the number the higher the noise!
