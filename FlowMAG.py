d78fea3178f313b7bea79695fe818f474ccf0bb9
Ricardos <ricardos.geral@gmail.com>
2018-11-30 17:40:34 +0000

    try to reconnect if FLOWMAG is not connected to USB serial port

--- FlowMAG.py
+++ FlowMAG.py
@@ -2,22 +2,35 @@ import serial
 import re
 from time import sleep
 import time
-import multiprocessing
-
 
 def read_flowMAG(v1,v2):
-    ser = serial.Serial(port='/dev/ttyACM0',
-                        baudrate=9600,
-                        parity=serial.PARITY_NONE,
-                        stopbits=serial.STOPBITS_ONE,
-                        bytesize=serial.EIGHTBITS,
-                        timeout=1.05)
+    try:
+          ser = serial.Serial(port='/dev/ttyACM0',
+                          baudrate=9600,
+                          parity=serial.PARITY_NONE,
+                          stopbits=serial.STOPBITS_ONE,
+                          bytesize=serial.EIGHTBITS,
+                          timeout=1.09)
+    except:
+        ser = None
+        print("MAG-flow not connected no USB! could not open port /dev/ttyACM0")
 
     volume = 0.0
     sleep(.2)
     first_time = time.time()
 
     while 1:
+        if ser == None:
+            try:
+                ser = serial.Serial(port='/dev/ttyACM0',
+                                    baudrate=9600,
+                                    parity=serial.PARITY_NONE,
+                                    stopbits=serial.STOPBITS_ONE,
+                                    bytesize=serial.EIGHTBITS,
+                                    timeout=1.09)
+            except:
+                pass
+        else:
             flow = ser.readline().decode()
             last_time = time.time()
             intv = last_time - first_time
@@ -29,4 +42,4 @@ def read_flowMAG(v1,v2):
             volume =volume + flow/(60*60*intv)  # in liters in each second (defined by time sleep)
             v1.value=flow
             v2.value=volume
-            sleep(1)
+        sleep(1)
